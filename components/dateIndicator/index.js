75c84fb98ae60916e226f76b2b74129bd054a7ad
Pedro <ph.calle@hotmail.com>
2023-02-24 13:32:19 -0300

    a lot

--- components/dateIndicator/index.js
+++ components/dateIndicator/index.js
@@ -1,13 +1,11 @@
-const { utcToZonedTime, format } = require("date-fns-tz");
+const { format } = require("date-fns");
 const { ptBR } = require("date-fns/locale");
 
 const DateIndicator = (date, options = "dd/LL/yyyy - HH:mm") => {
   const newDate = new Date(date);
-  const timeZone = "America/Sao_Paulo";
-  const zonedDate = utcToZonedTime(newDate, timeZone);
 
   if (date) {
-    return format(zonedDate, options, {
+    return format(newDate, options, {
       locale: ptBR,
       timeZone: "America/Sao_Paulo",
     });
