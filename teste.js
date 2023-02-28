1e5cd5c71cb2f9bfed921b583dde7b48c6e45131
Pedro <ph.calle@hotmail.com>
2023-02-28 15:25:08 -0300

    dl portio

--- teste.js
+++ teste.js
@@ -73,13 +73,36 @@
 // });
 
 const ffi = require("ffi-napi");
-
-const mydll = ffi.Library("MEGENCOM32.dll", {
-  MEGENCOM32_AbrirDispositivo: [
-    "string",
-    ["string", "int", "int", "int", "int", "int"],
-  ],
+const path = require("path");
+const mecom32 = ffi.Library(path.join(__dirname, 'MECOM32.dll'), {
+  // Defina as funções da DLL MECOM32.dll aqui
 });
+console.log('mecom32 found')
+const libprintio = ffi.Library(path.join(__dirname, 'LIBPRINTIO.dll'), {
+  // Defina as funções da DLL LIBPRINTIO.dll aqui
+});
+console.log('libprintio found')
 
-const result = mydll.MEGENCOM32_AbrirDispositivo("LPT1", 0, 0, 0, 0, 0);
+const dlportio = ffi.Library(path.join(__dirname, 'DLPortIO.DLL'), {
+
+});
+console.log('dlportio found')
+
+const mepar32 = ffi.Library(path.join(__dirname, 'MEPAR32.dll'), {
+  // Defina as funções da DLL MEPAR32.dll aqui
+}, {
+  'MECOM32.dll': mecom32,
+  'LIBPRINTIO.dll': libprintio,
+  'DLPortIO.DLL': dlportio,
+});
+console.log('mepar32 found')
+const megencom32 = ffi.Library(path.join(__dirname, 'MEGENCOM32.dll'), {
+  "MEGENCOM32_AbrirDispositivo": ['int', ['string', 'int', 'int', 'int', 'int', 'int']]
+}, {
+  // Lista de dependências necessárias e ordem em que devem ser carregadas
+  'MECOM32.dll': mecom32,
+  'MEPAR32.dll': mepar32,
+  'LIBPRINTIO.dll': libprintio
+});
+const result = megencom32.MEGENCOM32_AbrirDispositivo("LPT1", 0, 0, 0, 0, 0);
 console.log(result);
